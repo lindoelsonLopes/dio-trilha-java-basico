cec385802ad99ac576e6f6960fd2dc681f4f719a
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2023-01-06 10:33:30 -0300

    Usando operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,29 @@
+public class Operadores {
+    public static void main(String[] args) {
+      
+      // String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
+
+      // System.out.println(nomeCompleto);
+
+      // boolean casado = true;
+
+      // System.out.println(casado);
+
+      // casado = !casado;
+
+      // System.out.println(casado);
+
+      int a, b;
+      a = 5;
+      b = 5;
+
+      String resultado = "";
+
+      if(a==b) 
+       resultado = "Verdadeiro";
+      else
+       resultado = "Falso";
+      
+      System.out.println(resultado);
+    }
+  }
