179e714afa5ff588c6cd27a65ac71205cab3eb9b
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2023-01-06 11:12:17 -0300

    Usando alguns operadores lógicos

---
+++ tipos-variaveis/src/Operadores2.java
@@ -0,0 +1,18 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int numeroUm = 1;
+        int numeroDois = 2;
+
+        boolean simOuNao = numeroUm == numeroDois;
+
+        System.out.println("Numero 1 é igual á numero 2 ? " + simOuNao);
+
+        simOuNao = numeroUm > numeroDois;
+
+        System.out.println("Numero 1 é maior que numero 2 ? " + simOuNao);
+
+        simOuNao = numeroUm < numeroDois;
+
+        System.out.println("Numero 1 é maior que numero 2 ? " + simOuNao);
+    }
+}
