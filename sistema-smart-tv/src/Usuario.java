18b622edd12e422547516decb51203d7fe529d5c
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2023-01-06 12:51:13 -0300

    feat: Criando métodos para simular funcionamento de Tv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        System.out.println("Canal atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Novo canal agora é : " + smartTv.canal);
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal atual : " + smartTv.canal);
+        System.out.println("Volume atual : " + smartTv.volume); 
+        
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Status -> Volume agora é : " + smartTv.volume);
+    }
+
+}
